f6958019e5cc8275593688d446d973387a6eb94d
JavGuerra <javguerra@badared.com>
2022-04-18 21:42:27 +0200

    04

---
+++ js/04.js
@@ -0,0 +1,38 @@
+/*
+Fizz buzz
+=========
+    Imprime todos los números del 1 al 1000 por consola, con la salvedad de todos
+    aquellos números que sean múltiplos de 3 que en vez de imprimir el número, se
+    imprimirá fizz y además, en lugar de los números que sean múltiplos de 5 se
+    imprimirá buzz. Si el número es múltiplo de 3 y de 5, imprimir fizzbuzz.
+*/
+
+let resultado = "";
+
+for (let i = 1; i <= 100; i++) {
+
+  // Añade la coma y un espacio como separador
+  if (i > 1) {
+    resultado += ", ";
+  }
+
+  // ¿Es múltiplo de 3 y de 5?
+  if (i % 3 === 0 && i % 5 === 0) {
+
+    resultado += "fizzbuzz";
+
+  } else {
+
+    if (i % 3 === 0) {
+      resultado += "fizz";
+    } else if (i % 5 === 0) {
+      resultado += "buzz";
+    } else {
+      resultado += i;
+    }
+
+  }
+
+} // end for
+
+console.log(resultado);
