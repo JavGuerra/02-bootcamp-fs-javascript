731f5c061fe6938be1b084bfb82df166d2022766
JavGuerra <javguerra@badared.com>
2022-04-19 13:32:41 +0200

    04

--- js/04.js
+++ js/04.js
@@ -1,15 +1,15 @@
 /*
 Fizz buzz
 =========
-    Imprime todos los números del 1 al 1000 por consola, con la salvedad de todos
-    aquellos números que sean múltiplos de 3 que en vez de imprimir el número, se
-    imprimirá fizz y además, en lugar de los números que sean múltiplos de 5 se
-    imprimirá buzz. Si el número es múltiplo de 3 y de 5, imprimir fizzbuzz.
+  Imprime todos los números del 1 al 1000 por consola, con la salvedad de todos
+  aquellos números que sean múltiplos de 3 que en vez de imprimir el número, se
+  imprimirá fizz y además, en lugar de los números que sean múltiplos de 5 se
+  imprimirá buzz. Si el número es múltiplo de 3 y de 5, imprimir fizzbuzz.
 */
 
-let resultado = '';
+let resultado = '0';
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 1000; i++) {
   resultado += ', ';
 
   if (i % 3 == 0  ||  i % 5 == 0) {
@@ -21,4 +21,4 @@ for (let i = 1; i <= 100; i++) {
 
 } // end for()
 
-console.log('Números' + resultado);
+console.log (resultado);
