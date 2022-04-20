93918f5d2bde306c906b8e81d5cc680ba616d1a2
JavGuerra <javguerra@badared.com>
2022-04-21 01:44:20 +0200

    07

--- js/05-funciones.js
+++ js/05-funciones.js
@@ -10,96 +10,32 @@ número dado de veces
 si es primo y false si no lo es
 */
 
-/* -- Ejercicio 1 -- */
-
 function potenciacion(numero1, numero2 = 1) {
-  let resultado = null;
-  if (!isNaN(numero1) && !isNaN(numero2)) {
-    resultado = numero1 ** numero2;
-  }
-  return resultado;
+  return numero1 ** numero2;
 }
-
-// Anónimas autoejecutables
-(function (numero1, numero2 = 1) {console.log(numero1 ** numero2);})(2, 3); // Clásica
-((function (numero1, numero2 = 1) {console.log(numero1 ** numero2);})(2, 3)); // Crockford
-+function (numero1, numero2 = 1) {console.log(numero1 ** numero2);}(2, 3); // Unitaria
-!function (numero1, numero2 = 1) {console.log(numero1 ** numero2);}(2, 3); // Facebook
-
-// Flecha
-let potenciacion2 = (numero1, numero2 = 1) => numero1 ** numero2;
-
-((numero1, numero2 = 1) => console.log(numero1 ** numero2))(2, 3); // Flecha autoejecutable
-
-
-/* -- Ejercicio 2 -- */
 
 function repetirString(cadena, numero) {
-  let resultado = '';
-  if (!isNaN(numero)) {
-    numero = parseInt(numero);
-    if (numero > 0) {
-      for (let i = 1; i <= numero; i++) resultado += cadena;
-    }
+  numero = parseInt(numero);
+  let resultado = "";
+  if (numero > 0) {
+    for (let i = 1; i <= numero; i++) resultado += cadena;
   }
   return resultado;
 }
-
-// Flecha
-let repetirString2 = (cadena, numero) => {
-  let resultado = '';
-  if (!isNaN(numero)) {
-    numero = parseInt(numero);
-    if (numero > 0) {
-      for (let i = 1; i <= numero; i++) resultado += cadena;
-    }
-  }
-  return resultado;
-}
-
-let repetirString3 = (s, n) => s.repeat(n); // Borja
-
-
-/* -- Ejercicio 3 -- */
 
 function esPrimo(numero) {
-  let contador = 0; // Número de veces que el resto (%) es 0.
-  if (!isNaN(numero)) {
-    numero = parseInt(numero);
-    let i = 1;
-    while (i <= numero && contador <= 2) {
-      if (numero % i == 0) contador++;
-      i++;
-    }
+  numero = parseInt(numero);
+  let contador = 0;
+  for (let i = 1; i <= numero; i++) {
+    if (numero % i == 0) contador++;
   }
-  return contador == 2; // Divisible por 2 números: 1 y si mismo.
+  return contador == 2; // Divisble por 2 números: 1 y si mismo.
 }
 
-// Flecha
-
-let esPrimo2 = numero => {
-  let contador = 0; // Número de veces que el resto (%) es 0.
-  if (!isNaN(numero)) {
-    numero = parseInt(numero);
-    let i = 1;
-    while (i <= numero && contador <= 2) {
-      if (numero % i == 0) contador++;
-      i++;
-    }
-  }
-  return contador == 2; // Divisible por 2 números: 1 y si mismo.
-}
-
-
-/* -- Test -- */
+// Test
 
 console.log(potenciacion(2, 3));
-console.log(potenciacion2(3, 2));
 
-console.log(repetirString('Hola', 2));
-console.log(repetirString2('Hola', 3));
-console.log(repetirString3('Hola', 4));
+console.log(repetirString("Hola", 2));
 
-console.log(esPrimo(1));
-console.log(esPrimo2(3));
-
+console.log(esPrimo(2));
