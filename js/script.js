d81045f1c8bf673ea338961042c15b996e69e2e9
JavGuerra <javguerra@badared.com>
2022-04-27 17:23:15 +0200

    eventos

--- js/script.js
+++ js/script.js
@@ -6,18 +6,17 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
-const ul = document.getElementById('listado');
+const lista = document.getElementById('listado');
 
 fetch("js/listado.json")
 .then(resp => resp.json())
 .then(function(data) {
     return data.map(function(contenido) {
-        console.log(contenido);
-    //   let li = createNode('li');
-    //   let span = createNode('span');
-    //   span.innerHTML = `${contenido.titulo} - ${contenido.enlace}`;
-    //   append(li, span);
-    //   append(ul, li);
+        let div = createNode('div');
+        let span = createNode('span');
+        span.innerHTML = `${contenido.titulo} - ${contenido.enlace}`;
+        append(div, span);
+        append(lista, div);
     })
   })
 .catch(err => console.error(err))
