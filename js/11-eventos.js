033a61bb6bb0dfa4f6e8f1879b3e76c34df132f0
JavGuerra <javguerra@badared.com>
2022-04-27 03:33:03 +0200

    eventos

--- js/11-eventos.js
+++ js/11-eventos.js
@@ -15,39 +15,46 @@ Definiremos un script con los eventos:
 // document.getElementById('p1').addEventListener('click', rojo);
 // document.getElementById('p2').addEventListener('click', rojo);
 
-// let parrafos =  document.getElementsByTagName("p");
-// for (let i = 0; i < parrafos.length; i++) {
-//     parrafos[i].addEventListener('click', function() {this.style.color = 'red'});
-// };
+let parrafos = document.getElementsByTagName('p');
+for (let i = 0; i < parrafos.length; i++) {
+  parrafos[i].addEventListener('click', function () {
+    this.style.color = 'red';
+  });
+}
 
-document.querySelectorAll('p').forEach(
-    el => el.addEventListener('click', () => el.style.color = 'red') );
-
-document.getElementById('s1').addEventListener(
-    'click', function() {this.style.background = 'LightGreen'});
+document.getElementById('s1').addEventListener('click', function () {
+  this.style.background = 'LightGreen';
+});
 
 // function azul() {this.style.background = 'PaleTurquoise'};
 // document.getElementById('a1').addEventListener('click', azul);
 // document.getElementById('a2').addEventListener('click', azul);
 
-let articulos =  document.getElementsByTagName("article");
+let articulos = document.getElementsByTagName('article');
 for (let i = 0; i < articulos.length; i++) {
-    articulos[i].addEventListener('click', function() {
-        this.style.background = 'PaleTurquoise'
-    });
-};
+  articulos[i].addEventListener('click', function () {
+    this.style.background = 'PaleTurquoise';
+  });
+}
 
-document.getElementById('b1').addEventListener('click', function() {
-    document.getElementById('p1').innerText = 'Nuevo párrafo' });
+document.getElementById('b1').addEventListener('click', function () {
+  document.getElementById('p1').innerText = 'Nuevo párrafo';
+});
 
 // function imagen() {this.setAttribute('src', 'img/foto3.jpg')};
 // document.getElementById('i1').addEventListener('click', imagen);
 // document.getElementById('i2').addEventListener('click', imagen);
 
-let imagenes =  document.getElementsByTagName("img");
+let imagenes = document.getElementsByTagName('img');
 for (let i = 0; i < imagenes.length; i++) {
-    imagenes[i].addEventListener('click', function() {
-        this.setAttribute('src', 'img/foto3.jpg')
-    });
-};
+  imagenes[i].addEventListener('click', function () {
+    this.setAttribute('src', 'img/foto3.jpg');
+  });
+}
 
+document.querySelectorAll('img').forEach(
+    el => el.addEventListener('click', e =>
+      e.currentTarget.setAttribute('src', 'img/foto3.jpg'))
+  );
+
+  // .currentTarget
