63191905252d0d482af16b2ffa1a2099749af002
JavGuerra <javguerra@badared.com>
2022-05-04 16:04:50 +0200

    nodos

--- js/mosaico.js
+++ js/mosaico.js
@@ -1,13 +1,13 @@
-/* Mosaico, (CC BY) Javier Guerra, mayo 2022 */
+/* «Mosaico» (CC BY) Javier Guerra, mayo 2022 */
 
-let btnNegro    = elemento('#btnNegro');
-let btnAzul     = elemento('#btnAzul');
-let btnRojo     = elemento('#btnRojo');
-let btnMagenta  = elemento('#btnMagenta');
-let btnVerde    = elemento('#btnVerde');
-let btnCian     = elemento('#btnCian');
+let btnNegro    = elemento('#btnNegro'   );
+let btnAzul     = elemento('#btnAzul'    );
+let btnRojo     = elemento('#btnRojo'    );
+let btnMagenta  = elemento('#btnMagenta' );
+let btnVerde    = elemento('#btnVerde'   );
+let btnCian     = elemento('#btnCian'    );
 let btnAmarillo = elemento('#btnAmarillo');
-let btnBlanco   = elemento('#btnBlanco');
+let btnBlanco   = elemento('#btnBlanco'  );
 
 btnNegro.onclick    = color;
 btnAzul.onclick     = color;
@@ -18,15 +18,16 @@ btnCian.onclick     = color;
 btnAmarillo.onclick = color;
 btnBlanco.onclick   = color;
 
-let numCols = 16; // Columnas de la tabla
-let numFils = 16; // Filas de la tabla
-let btnSel  = btnNegro; // Botón seleccionado
-let pincel  = btnSel.className; // Color del pincel
-btnSel.classList.add('seleccion');
-
+let numCols = 16;
+let numFils = 16;
+let btnSel  = btnNegro; // Selección Btn
+let pincel  = btnSel.className; // Color
 let padre   = elemento('main');
 let tabla   = creaTabla(numCols, numFils);
+
+btnSel.classList.add('seleccion');
 padre.appendChild(tabla);
+
 
 function elemento(sel) { return document.querySelector(sel); }
 
@@ -34,24 +35,23 @@ function creaElem(el)  { return document.createElement(el);  }
 
 function creaTabla(numCols, numFils) {
     let tabla, tbody, tr, td;
-    // tbody
     tbody = creaElem('tbody');
     for (let i = 1; i <= numFils; i++) {
         tr = creaElem('tr');
         for (let i = 1; i <= numCols; i++) {
             td = creaElem('td');
             td.classList.add('blanco');
-            td.onclick = pinta; // Cambia color celda
+            td.onclick = pinta; // ¡Magia!
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
     }
-    // table
     tabla = creaElem('table');
     tabla.appendChild(tbody);
     return tabla;
 }
 
+// Selecciona botón y color del pincel
 function color() {
     btnSel.classList.remove('seleccion');
     btnSel = this;
@@ -59,4 +59,5 @@ function color() {
     btnSel.classList.add('seleccion');
 }
 
+// Pone el color del pincel en la celda
 function pinta() { this.className = pincel; }
