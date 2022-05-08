0c8ad3923bb529a2c3e427c96e6b83d778f3b157
JavGuerra <javguerra@badared.com>
2022-05-08 22:15:22 +0200

    formulario

---
+++ js/13-formulario.js
@@ -0,0 +1,8 @@
+/*
+Cogemos el ejercicio del formulario de cuando dimos html. Cuando le des al
+botón de enviar formulario capturas el evento y se validan los campos del
+formulario. Si los datos están todos bien, presentar los datos. El ejemplo que
+puso fue una ficha personal, pero dijo que podía hacerse de la forma que se
+quiera. Una vez este la funcionalidad esa hecha, si da tiempo, aplicar Flexbox
+al formulario para dejarlo medio bonito y sin los br tan feos que usamos en su día.
+*/
