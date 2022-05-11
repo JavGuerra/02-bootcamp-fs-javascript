5a1a8eb62d73f4226e424ad46d09a1a27f97da51
JavGuerra <javguerra@badared.com>
2022-05-11 17:56:03 +0200

    localstorage

--- js/15-localstorage.js
+++ js/15-localstorage.js
@@ -21,5 +21,95 @@ Usa JSON.parse() y JSON.stringify() para guardar muchos datos usando la misma cl
 */
 
 
-let fechaAlta;
-let datosForm;
+const enviar  = elemento('#enviar');
+const borrLS  = elemento('#borrLS');
+const borraF  = elemento('#borraF');
+const datos   = elemento('#datos' );
+const fecha   = elemento('#fecha' );
+const form    = document.formulario;
+let requerido = {nombre: false, edad: false, correo: false};
+
+// Eventos de crear
+form.nombre.onchange = validaRequeridos;
+form.edad.onchange   = validaRequeridos;
+form.correo.onchange = validaRequeridos;
+form.onsubmit = guardaLS;
+
+// Eventos de borrar
+borrLS.onclick = borraLS;
+borraF.onclick = borraFh;
+
+// Inicio
+poneEstado();
+
+
+function elemento(sel)  { return document.querySelector(sel); }
+
+function poneEstado() {
+    let hayDatos = (existeClave('datos') || existeClave('fecha')) ? true : false;
+
+    // Schrödinger
+    elemento('#formulario').style.display = !hayDatos ? 'initial' : 'none';
+    elemento('#resultado').style.display  =  hayDatos ? 'initial' : 'none';
+
+    if (!hayDatos) {
+        enviar.disabled  = true;
+        requerido.nombre = requerido.edad = requerido.correo = false;
+        form.reset();
+    } else {
+        borrLS.disabled  = false;
+        borraF.disabled  = false;
+        recogeLS();
+    }
+}
+
+function validaRequeridos() {
+    requerido.nombre = form.nombre.value ? true : false; 
+    requerido.edad   = form.edad.value   ? true : false; 
+    requerido.correo = form.correo.value ? true : false;
+
+    enviar.disabled  = !(requerido.nombre && requerido.edad && requerido.correo);
+}
+
+function guardaLS(evento) {
+    evento.preventDefault();
+
+    requerido.nombre = form.nombre.value;
+    requerido.edad   = form.edad.value;
+    requerido.correo = form.correo.value;
+
+    localStorage.datos = JSON.stringify(requerido);
+    localStorage.fecha = new Date();
+
+    poneEstado();
+}
+
+function recogeLS() {
+    datos.textContent = existeClave('datos') ? localStorage.datos : 'no existe';
+    fecha.textContent = existeClave('fecha') ? localStorage.fecha : 'no existe';
+
+    if (existeClave('datos')) {
+        datos.innerHTML += '<br />Propiedades del objeto «datos»:';
+        requerido = JSON.parse(localStorage.datos);
+        for (const propiedad in requerido) {
+            datos.innerHTML += `<br /><span class="etiqueta">${propiedad}:</span> `;
+            datos.innerHTML += `${requerido[propiedad]}`;
+        }
+    }
+}
+
+function borraLS() {
+    borrLS.disabled = true;
+    if (localStorage.length) localStorage.clear();
+    poneEstado();
+}
+
+function borraFh() {
+    borraF.disabled = true;
+    if (existeClave('fecha')) localStorage.removeItem('fecha');
+    fecha.textContent = 'no existe';
+}
+
+function existeClave(clave) {
+    return localStorage.getItem(clave) !== undefined && localStorage.getItem(clave);
+}
