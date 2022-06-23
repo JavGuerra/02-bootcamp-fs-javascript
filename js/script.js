69009b7ff04c585365b6d623ed2959785f7ac3ab
JavGuerra <javguerra@badared.com>
2022-06-23 10:57:43 +0200

    script

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 fetch('js/listado.json')
-.then(resp => resp.json())
-.then(data => {
+  .then(resp => resp.json())
+  .then(data => {
     const lista = document.getElementById('listado');
     let article;
     for (tarea of data) {
         article = document.createElement('article');
         article.innerHTML =
-         `<a target="_blank" href="${tarea.enlace}">${tarea.titulo}</a>`;
+        `<a target="_blank" href="${tarea.enlace}">${tarea.titulo}</a>`;
         lista.appendChild(article);
     }
   })
-.catch(err => console.error(err))
+  .catch(err => console.error(err))
